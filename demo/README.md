cfc4b9eeb55e0b5be5c60e34c5eb715fba977a3a
zrda27@163.com <da901213>
2016-08-23 20:39:35 +0800

    实现less、sass的编译

--- demo/README.md
+++ demo/README.md
@@ -1,14 +1,20 @@
-1 安装express
+**1 安装express**
     1.1 npm install -g express-generator
     1.2 express -e demo
     1.3 cd demo && npm install
 
-2 安装gulp
+**2 安装插件**
     npm install -g gulp --save-dev
     npm install gulp-uglify --save-dev
     npm install gulp-concat --save-dev
     npm install gulp-rename --save-dev
     npm install gulp-minify-css --save-dev
     npm install del --save-dev
+    npm install gulp-imagemin  --save-dev
+    npm install gulp-less --save-dev
+    npm install gulp-sass --save-dev
+    npm install node-sass -g
+    npm install gulp-util --save-dev
 
-3 执行任务：gulp taskName
+**3 执行任务**
+    gulp taskName
--- demo/gulpfile.js
+++ demo/gulpfile.js
@@ -1,19 +1,49 @@
 /**
  * Created by Administrator on 2016-08-23.
  */
+var projectName = "demo";
+
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var minifyCSS = require('gulp-minify-css');
 var del = require('del');
-var projectName = "demo";
+var imagemin = require('gulp-imagemin');
+var less = require('gulp-less');
+var sass = require('gulp-sass');
+var gutil = require('gulp-util')
+
+gulp.task('default', function () {
+    gutil.log('message')
+    gutil.log(gutil.colors.red('error'))
+    gutil.log(gutil.colors.green('message:') + "some")
+})
+
+gulp.task('default', ["cleanDest", "minjs", "less", "sass", "mincss", 'minimg']);
+
+gulp.task('cleanDest', function(cb) {
+    del(['public/css', 'public/js', 'public/images'], cb)
+});
+gulp.task('cleanCssTmp', function(cb) {
+    del(['src/css/tmp'], cb)
+});
+
+// 在命令行使用 gulp auto 启动此任务
+gulp.task('auto', function () {
+    // 监听文件修改，当文件被修改则执行 script 任务
+    gulp.watch('src/**/*.js', ['minjs'])
+    // 监听文件修改，当文件被修改则执行 css 任务
+    gulp.watch('src/**/*.css', ['mincss'])
+    // 监听文件修改，当文件被修改则执行 image 任务
+    gulp.watch('src/images/**/*.*', ['minimg'])
+})
 
 // 压缩 js 文件
 // 在命令行使用 gulp script 启动此任务
 gulp.task('minjs', function() {
     // 1. 找到文件
-    gulp.src(['src/*.js', 'src/*/*.js', 'src/*/*/*.js'])
+    gulp.src('src/**/*.js')
         .pipe(concat("demo" + '.js'))    //合并所有js到main.js
         .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
         .pipe(uglify())    //压缩
@@ -24,25 +54,46 @@ gulp.task('minjs', function() {
 // 在命令行使用 gulp css 启动此任务
 gulp.task('mincss', function () {
     // 1. 找到文件
-    gulp.src(['src/*.css', 'src/*/*.css', 'src/*/*/*.css'])
+    gulp.src('src/**/*.css')
         .pipe(concat("demo" + '.css'))    //合并所有js到main.js
         .pipe(rename({suffix: '.min'}))   //rename压缩后的文件名
         // 2. 压缩文件
         .pipe(minifyCSS())
         // 3. 另存为压缩文件
         .pipe(gulp.dest('public/css'))
-})
-
-gulp.task('clean', function(cb) {
-    del(['public/css', 'public/js'], cb)
 });
 
-// 在命令行使用 gulp auto 启动此任务
-gulp.task('auto', function () {
-    // 监听文件修改，当文件被修改则执行 script 任务
-    gulp.watch(['src/*.js', 'src/*/*.js', 'src/*/*/*.js'], ['minjs'])
-    // 监听文件修改，当文件被修改则执行 css 任务
-    gulp.watch(['src/*.css', 'src/*/*.css', 'src/*/*/*.css'], ['mincss'])
-})
+// 压缩图片任务
+// 在命令行输入 gulp images 启动此任务
+gulp.task('minimg', function () {
+    // 1. 找到图片
+    gulp.src('src/images/**/*.*')
+    // 2. 压缩图片
+        .pipe(imagemin({
+            progressive: true
+        }))
+        // 3. 另存图片
+        .pipe(gulp.dest('public/images'))
+});
 
-gulp.task('default', ["clean", "minjs", "mincss"])
+// 编译less
+// 在命令行输入 gulp less 启动此任务
+gulp.task('less', function () {
+    // 1. 找到 less 文件
+    gulp.src('src/less/**/**.less')
+    // 2. 编译为css
+        .pipe(less())
+        // 3. 另存文件
+        .pipe(gulp.dest('src/css/tmp/less'))
+});
+
+// 编译sass
+// 在命令行输入 gulp sass 启动此任务
+gulp.task('sass', function () {
+    // 1. 找到 less 文件
+    gulp.src('src/sass/**/**.sass')
+    // 2. 编译为css
+        .pipe(sass())
+        // 3. 另存文件
+        .pipe(gulp.dest('src/css/tmp/sass'))
+});
